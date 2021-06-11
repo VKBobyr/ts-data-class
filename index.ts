f2e7092fafc2557e958d3556c88f4368677999db
Volodymyr Bobyr <vkbobyr@Outlook.com>
2021-06-10 22:09:36 -0400

    set up package.json for publishing to npm and added exports

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-export { };
+import DClass from './src/data_class';
+import { NumberParser, StringArrayParser, StringParser } from './src/parsers';
+
+export default DClass;
+export {
+  StringParser,
+  NumberParser,
+  StringArrayParser,
+};
