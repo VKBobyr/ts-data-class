db7b99b6f98142699793f9ba857b6909bfe46b84
Volodymyr Bobyr <vkbobyr@Outlook.com>
2021-06-15 17:16:33 -0400

    Removed ts-is-defined from dependencies due to nextjs compilation issues

--- src/parsers/array_filtered_parser.ts
+++ src/parsers/array_filtered_parser.ts
@@ -1,11 +1,10 @@
-import { isDefined } from 'ts-is-defined';
 import { Parser } from './parsers';
 
 function ArrayFilteredParser<T>(itemParser: Parser<T>): Parser<NonNullable<T>[] | undefined> {
   return (arr: unknown) => {
     if (!Array.isArray(arr)) return undefined;
     const mapped = arr.map((v: unknown) => itemParser(v));
-    return mapped.filter(isDefined);
+    return mapped.filter((v) => v !== undefined && v !== null) as NonNullable<T>[];
   };
 }
 
--- test/parsers/array_filtered_parser.test.ts
+++ test/parsers/array_filtered_parser.test.ts
@@ -5,7 +5,7 @@ import ArrayFilteredParser from '../../src/parsers/array_filtered_parser';
 describe('ArrayFilteredParser', () => {
   it('should filter items that fail the parse', () => {
     const items = [0, 1, 2, 3, 4, 5];
-    const filter = ArrayFilteredParser((v) => (v > 3 ? v : undefined));
-    expect(filter(items)).to.have.same.members([4, 5]);
+    const filter = ArrayFilteredParser((v) => (v < 3 ? v : undefined));
+    expect(filter(items)).to.have.same.members([0, 1, 2]);
   });
 });
